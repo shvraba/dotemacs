65c43d392448bd01e888e3a7498973f515df24f2
Geoff Wark <gtw@cock.li>
2017-07-08 00:10:38 -0400

    Add header/footer

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
+;;; init.el --- The first thing GNU Emacs runs
+
 ;; Keep all temporary files in system's $TMPDIR.  This reduces file clutter.
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;;; init.el ends here
