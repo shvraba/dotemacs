0d9eb3fc602f0dd090e0008656410d716a51822e
Geoff Wark <gtw@cock.li>
2017-07-12 23:10:22 -0400

    Ignore default REGEXP checks at startup

--- init.el
+++ init.el
@@ -5,17 +5,22 @@
 ;; NOTE: We reset this value at the end of the config.
 (setq gc-cons-threshold 999999999999)
 
-;; Keep all temporary files in system's $TMPDIR.  This reduces file clutter.
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+;; Ignore default REGEXP checks of file names at startup.
+;; This also drastically improves `emacs-init-time'. SOURCE= https://redd.it/3kqt6e
+;; NOTE: Some bogus, benign errors about packages/directories may be thrown due to this...
+(let ((file-name-handler-alist nil))
 
-;; Revert garbage collection behaviour to a normal, more modern level.
-;; SOURCE= https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/cv059dc/
-(run-with-idle-timer
- 5 nil
- (lambda ()
-   (setq gc-cons-threshold 20000000))) ; magnars' recommendation
+  ;; Keep all temporary files in system's $TMPDIR.  This reduces file clutter.
+  (setq backup-directory-alist
+	`((".*" . ,temporary-file-directory)))
+  (setq auto-save-file-name-transforms
+	`((".*" ,temporary-file-directory t)))
+
+  ;; Revert garbage collection behaviour to a normal, more modern level.
+  ;; SOURCE= https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/cv059dc/
+  (run-with-idle-timer
+   5 nil
+   (lambda ()
+     (setq gc-cons-threshold 20000000)))) ; magnars' recommendation
 
 ;;; init.el ends here
